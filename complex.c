82f3f05fc5977be415282ca91260689860a6883f
EMROF-H <393575404@qq.com>
2022-07-06 09:44:18 +0800

    Create complex.c

---
+++ complex.c
@@ -0,0 +1,31 @@
+#include "complex.h"
+
+ComplexNumber TwiddleFactor(int power)
+{
+    ComplexNumber result = {cos(-TAU*power/LENGTH),sin(-TAU*power/LENGTH)};
+    return result;
+}
+
+ComplexNumber ComplexAdd(ComplexNumber a,ComplexNumber b)
+{
+    ComplexNumber result;
+    result.real      = a.real+b.real;
+    result.imaginary = b.imaginary+a.imaginary;
+    return result;
+}
+
+ComplexNumber ComplexReduce(ComplexNumber a,ComplexNumber b)
+{
+    ComplexNumber result;
+    result.real      = a.real-b.real;
+    result.imaginary = b.imaginary-a.imaginary;
+    return result;
+}
+
+ComplexNumber ComplexMultiply(ComplexNumber a,ComplexNumber b)
+{
+    ComplexNumber result;
+    result.real      = a.real*b.real-a.imaginary*b.imaginary;
+    result.imaginary = a.real*b.imaginary+a.imaginary*b.real;
+    return result;
+}
